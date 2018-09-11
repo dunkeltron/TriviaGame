912d7afe428a5c2d2e875ced02e7c084037048cf
Ian Dunkelberg <idunkelberg@gmail.com>
2018-09-11 12:47:46 -0700

    started work on javascript for game logic.html and css good enough for now

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var questionObj={
+    question='',
+    answers:['','','',''],
+    correctAnswer:0
+}
+var questions=["Who is the leader of the Autobots",
+                "Who is the leader of the Decepticons",
+                "Who is the main antagonist in The Transformers: The Movie"];
+var answers=[
+    ["Jazz","Optimus Rhyme","Optimus Prime","R2-D2"],
+    ["Starscrream","Soundwave","Shockwave","Megatron"],
+    ["rust","Unicron","Spike","The Galactic Empire"]
+];
+var correctAnswers=[2,3,1];
